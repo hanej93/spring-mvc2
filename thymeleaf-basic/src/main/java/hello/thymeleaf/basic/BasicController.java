0da1a99318f38601467d14f25f105d65e6155be8
EJ <hanej93@naver.com>
2023-05-03 20:01:46 +0900

    속상 값 설정

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -96,6 +96,11 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute(Model model) {
+        return "basic/attribute";
+    }
+
     @Data
     @AllArgsConstructor
     static class User {
