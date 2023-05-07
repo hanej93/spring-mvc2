3d9c2ed80c51ba7026885ab3610e2539ad9def4a
EJ <hanej93@naver.com>
2023-05-08 02:14:43 +0900

    서블릿 필터 - 요청 로그

---
+++ login/src/main/java/hello/login/web/login/WebConfig.java
@@ -0,0 +1,21 @@
+package hello.login.web.login;
+
+import hello.login.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean loginFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+}
